20ea4b191c470f520661a7932c2604b7601fc9ed
ChristineHu520 <1760812359@qq.com>
2021-03-31 09:14:50 +0800

    20210331

---
+++ 进阶课本练习项目/项目7-自建公告板/再次实现/message_sqlcreate.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+# @Time    : 2021/3/30 16:39
+# @Author  : ChristineHu
+"""
+import sqlite3
+
+conn = sqlite3.connect('messages.db')
+curs = conn.cursor()
+
+curs.execute('''
+create table messages(
+id integer primary key autoincrement ,
+subject text not null ,
+sender text not null ,
+reply_no int,
+text text not null 
+)''')
+
+conn.commit()
+conn.close()

47ad96ccfbddb3f6311c5cf4be6dc3c1586390f9
ChristineHu520 <1760812359@qq.com>
2021-03-31 17:26:14 +0800

    20210331

--- 进阶课本练习项目/项目8-使用XML-RPC/初次实现/simple_node.py
+++ 进阶课本练习项目/项目8-使用XML-RPC/初次实现/simple_node.py
@@ -4,7 +4,7 @@
 # @Author  : ChristineHu
 """
 
-from  xmlrpc.client import ServerProxy
+from xmlrpc.client import ServerProxy
 from os.path import join, isfile
 from xmlrpc.server import SimpleXMLRPCServer
 from urllib.parse import urlparse
@@ -16,16 +16,19 @@ OK = 1
 FAIL = 2
 EMPTY = ''
 
+
 def get_port(url):
 	'从URL中提取端口'
-	name =urlparse(url)[1]
+	name = urlparse(url)[1]
 	parts = name.split(':')
 	return int(parts[-1])
+
 
 class Node:
 	"""
 	P2P网络中的节点
 	"""
+
 	def __init__(self, url, dirname, secret):
 		self.url = url
 		self.dirname = dirname
@@ -68,7 +71,7 @@ class Node:
 			return FAIL
 		code, data = self.query(query)
 		if code == OK:
-			f = open(join(self.dirname, query),' w')
+			f = open(join(self.dirname, query), ' w')
 			f.write(data)
 			f.close()
 			return OK
@@ -80,7 +83,7 @@ class Node:
 		供内部来启动XML_RPC服务器
 		:return:
 		"""
-		s = SimpleXMLRPCServer(("",get_port(self.url)), logRequests=False)
+		s = SimpleXMLRPCServer(("", get_port(self.url)), logRequests=False, allow_none=True)
 		s.register_instance(self)
 		s.serve_forever()
 
@@ -115,11 +118,17 @@ class Node:
 				self.known.remove(other)
 			return FAIL, EMPTY
 
+
 def main():
-		url, directory, secret = sys.argv[1:]
-		n = Node(url, directory, secret)
-		n._start()
+	url, directory, secret = sys.argv[1:]
+	n = Node(url, directory, secret)
+	n._start()
 
 
 if __name__ == '__main__':
-    main()
+	main()
+
+# from xmlrpc.client import *
+# mpeer = ServerProxy('http://localhost:4242')
+# code, data = mpeer.query('test.txt')
+# code

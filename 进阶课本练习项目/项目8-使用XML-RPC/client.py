6af8fb66c9860aa5ab6d624067f237b161570724
ChristineHu520 <1760812359@qq.com>
2021-04-14 19:47:48 +0800

    20210401

--- 进阶课本练习项目/项目8-使用XML-RPC/client.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-# @Time    : 2021/4/1 11:22
-# @Author  : ChristineHu
-"""
-from xmlrpc.client import *
-mpeer = ServerProxy('http://localhost:4243')
-code, data = mpeer.query('test.txt')
-print(code)
